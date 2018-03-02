92dba878f7ca5dc746fe071cc61cd77e5a040c4b
Andy Evans <andrewevans0@outlook.com>
2018-03-02 11:36:31 +0000

    first commti

---
+++ .build/src/handlers/requestLogger.js
@@ -0,0 +1,19 @@
+const launch = require('@serverless-chrome/lambda')
+
+const handler = require('./244feoqxiy7___requestLogger.js')
+const options = {"flags":["--window-size=1280x1696","--hide-scrollbars"],"chromePath":"/var/task/headless-chromium"}
+
+module.exports.default = function ensureHeadlessChrome (event, context, callback) {
+  (typeof launch === 'function' ? launch : launch.default)(options)
+    .then((instance) => {
+      handler.default(event, context, callback, instance)
+    })
+    .catch((error) => {
+      console.error(
+        'Error occured in serverless-plugin-chrome wrapper when trying to ' +
+          'ensure Chrome for default() handler.',
+        options,
+        error
+      )
+    })
+}
